cd62313a177a2936944f4d39fa06841bbc323892
Your Name <your_email@example.com>
2024-09-08 15:05:24 +0800

    12036860 lab session

--- file 1.py
+++ file 1.py
@@ -1,1 +1,23 @@
-print("hello world") 
+a= 15 
+b=12
+print(type(a))
+print(type(b))
+print("a+b=",a+b)
+print(f"a-b={a-b}")
+print("a*b=",a*b)
+print(f"a/b={a/b}")
+c=int(a/b)
+print(c)
+print(type (c))
+print(float(c))
+b=float(c)
+print(type(b))
+print(f"The result of a divided by b is:{c}")
+if a>b:
+    print("true")
+else:
+    print("false")
+if a==b:
+    print("true")
+else:
+    print("false")
